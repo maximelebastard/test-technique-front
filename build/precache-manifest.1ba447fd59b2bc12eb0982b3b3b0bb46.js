ef56c90f33d9f59157f147dc590d52d37f928619
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-23 16:35:56 +0000

    Deploy maximelebastard/test-technique-front to github.com/maximelebastard/test-technique-front.git:gh-pages

---
+++ build/precache-manifest.1ba447fd59b2bc12eb0982b3b3b0bb46.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be665f6d246c215b1d047756698003f",
+    "url": "/test-technique-front/index.html"
+  },
+  {
+    "revision": "b15ac9f4e4a6d1d971a7",
+    "url": "/test-technique-front/static/css/main.1fada910.chunk.css"
+  },
+  {
+    "revision": "3639c284edeeda1c37e9",
+    "url": "/test-technique-front/static/js/2.34fc2767.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/test-technique-front/static/js/2.34fc2767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b15ac9f4e4a6d1d971a7",
+    "url": "/test-technique-front/static/js/main.d0ec29ad.chunk.js"
+  },
+  {
+    "revision": "27d2a88523ac95a0d64d",
+    "url": "/test-technique-front/static/js/runtime-main.e43d3732.js"
+  },
+  {
+    "revision": "0e5d0ff7868b3bf6c33607c18da6a87b",
+    "url": "/test-technique-front/static/media/dropdown-arrow.0e5d0ff7.svg"
+  },
+  {
+    "revision": "8fa0723a27fbd272777414d1ccf5e183",
+    "url": "/test-technique-front/static/media/icon-star-empty.8fa0723a.svg"
+  },
+  {
+    "revision": "19a5327631aae083f924c213216a6ce1",
+    "url": "/test-technique-front/static/media/icon-star-full.19a53276.svg"
+  },
+  {
+    "revision": "1173fcab5a8cffd7cf699bfc60d91859",
+    "url": "/test-technique-front/static/media/logo.1173fcab.svg"
+  },
+  {
+    "revision": "beae07eb2777b33260030250165649d9",
+    "url": "/test-technique-front/static/media/undraw_may_the_force.beae07eb.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97eaa307a243d07a5eff33194dfa7f8e.js"
+  "/test-technique-front/precache-manifest.1ba447fd59b2bc12eb0982b3b3b0bb46.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-technique-front/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
